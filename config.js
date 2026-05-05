4401d4411d1fa941ad2c7111f6bb4b8acda14aed
Lumenspro <etslumenspro@gmail.com>
2026-05-05 22:35:44 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCyYZluEmcM4lXiYIkg9g6v4_-k7O5M0LU",
+  authDomain: "cours-lumenspro.firebaseapp.com",
+  projectId: "cours-lumenspro",
+  storageBucket: "cours-lumenspro.firebasestorage.app",
+  messagingSenderId: "210429063963",
+  appId: "1:210429063963:web:8c6c60ec6a93828d84d9be"
+    },
+    whatsapp: "22891532410",
+    tel: "+228 91 53 24 10",
+    email: "etslumenspro@gmail.com",
+    nom: "LUMEN S-PRO SERVICES",
+    ville: "Vogan",
+    annee: "2026"
+};
